a129af5a2c4645fba3a9cff2d1a38b962126f089
Anjar Tiyo <anjar@feedloop.ai>
2024-01-04 13:54:45 +0700

    Add initial backbone

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,32 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({ message: 'Hello Bun!' })
 })
 
-export default app
+app.post('/run-command', async (c) => {
+  const {command} = await c.req.json();
+
+  if(!command){
+    c.status(400);
+    return c.json({
+      status: "ERROR",
+      result: "ERROR",
+      message: "command cannot be empty"
+    })
+  }
+
+  return c.json({
+    status: "SUCCESS",
+    result: "PASSED",
+    command: command
+  })
+})
+
+const port = parseInt(process.env.PORT!) || 3001
+console.log(`Running at http://localhost:${port}`)
+
+export default {
+  port,
+  fetch: app.fetch,
+}
