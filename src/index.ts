21e50ca0a519c8f7b26ad8ca8a882cb162e074db
Anjar Tiyo <anjar@feedloop.ai>
2024-01-04 15:57:26 +0700

    Add execute command

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,51 @@
-import { Hono } from 'hono'
+import { exec } from "child_process";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Bun!' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Hello Bun!" });
+});
 
-app.post('/run-command', async (c) => {
-  const {command} = await c.req.json();
+app.post("/run-command", async (c) => {
+  const { command } = await c.req.json();
 
-  if(!command){
+  if (!command) {
     c.status(400);
     return c.json({
       status: "ERROR",
       result: "ERROR",
-      message: "command cannot be empty"
-    })
+      message: "command cannot be empty",
+    });
   }
 
-  return c.json({
-    status: "SUCCESS",
-    result: "PASSED",
-    command: command
-  })
-})
+  try {
+    const result = await executeCommand(command);
+    return c.json({
+      status: "SUCCESS",
+      result,
+    });
+  } catch (err) {
+    return c.json({ error: err });
+  }
+});
 
-const port = parseInt(process.env.PORT!) || 3001
-console.log(`Running at http://localhost:${port}`)
+const executeCommand = (command: string) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        // console.log(stdout);
+        resolve(stdout.trim());
+      }
+    });
+  });
+};
+
+const port = parseInt(process.env.PORT!) || 3001;
 
 export default {
   port,
   fetch: app.fetch,
-}
+};
